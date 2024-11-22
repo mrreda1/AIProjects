5dee3cd31c1d8a5b58efab954b3dd15962b0db6d
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-11-22 20:43:32 +0200

    Better readability - backtracking

--- N-Queens/algorithms.py
+++ N-Queens/algorithms.py
@@ -1,4 +1,8 @@
 class Backtracking:
+    """ Solves the N-Queens problem using the backtracking algorithm.
+    Attributes:
+        n: The size of the chessboard (NxN). """
+
     queens = []
 
     def __init__(self, n):
@@ -8,12 +12,11 @@ class Backtracking:
         self.occupied_revdiagonal = [False] * n * 2
 
     def run(self):
-        self.solve()
-        return self.queens
+        return self.queens if self.solve() else None
 
     def solve(self, y=0):
         if len(self.queens) == self.n:
-            return
+            return True
         for x in range(self.n):
             ok = not (self.occupied_column[x] or self.occupied_diagonal[y + x]
                       or self.occupied_revdiagonal[y - x + self.n])
@@ -21,9 +24,9 @@ class Backtracking:
                 self.occupied_column[x] = self.occupied_diagonal[y + x] = \
                     self.occupied_revdiagonal[y - x + self.n] = True
                 self.queens.append(x)
-                self.solve(y + 1)
-                if len(self.queens) == self.n:
-                    return
+                if self.solve(y + 1):
+                    return True
                 self.queens.pop()
                 self.occupied_column[x] = self.occupied_diagonal[y + x] = \
                     self.occupied_revdiagonal[y - x + self.n] = False
+        return False
