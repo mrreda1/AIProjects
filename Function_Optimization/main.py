81ea8d7f6ff8221daea97a1acf169ab0fff0b728
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-11-28 17:34:11 +0200

    Optimize the code to make it 100x faster

--- Function_Optimization/main.py
+++ Function_Optimization/main.py
@@ -43,12 +43,10 @@ def natural_selection(population: Population, k: int,
     return population[:k]
 
 
-def select_parents(population: Population,
-                   fitness_func: FitnessFunc) -> List[Genome]:
+def select_parents(population: Population) -> List[Genome]:
     """Select two parents for the new genomes"""
 
-    weights = [fitness_func(genome) for genome in population]
-    return random.choices(population=population, weights=weights, k=2)
+    return random.choices(population=population, k=2)
 
 
 def crossover(parents: List[Genome], k: int) -> List[Genome]:
@@ -71,14 +69,12 @@ def mutation(genome: Genome) -> Genome:
     return genome
 
 
-def make_generation(population: Population,
-                    fitness_func: FitnessFunc) -> Population:
+def make_generation(population: Population) -> Population:
     """Make new generation from current generation"""
 
     generation = []
     for _ in range(400):
-        parents = select_parents(
-            population=population, fitness_func=fitness_func)
+        parents = select_parents(population=population)
         generation += crossover(parents=parents, k=3)
 
     for i, _ in enumerate(generation):
@@ -99,21 +95,22 @@ def is_good_enough(solution: Genome) -> bool:
 def main():
     """Driver function"""
 
+    gen = 0
     population = generate_population(length=1200)
-    for i in range(10000):
+    while True:
         population = natural_selection(
             population=population, k=100, fitness_func=fitness)
 
         best_solution = population[0]
 
-        print(f"Generation {i}'s best solution is {
+        print(f"Generation {gen}'s best solution is {
               fitness(best_solution)}, {best_solution}")
 
         if is_good_enough(best_solution):
             return
 
-        population = make_generation(
-            population=population, fitness_func=fitness)
+        gen += 1
+        population = make_generation(population=population)
 
 
 if __name__ == "__main__":
