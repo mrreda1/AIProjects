d3fd359ff7c8c1d736854100dd7127a64b6e8141
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2024-11-29 07:33:59 +0200

    Update

--- Function_Optimization/main.py
+++ Function_Optimization/main.py
@@ -1,117 +1,140 @@
 import random
-import math
+from math import inf
 from typing import List, Callable
+import matplotlib.pyplot as plt
 
 Genome = List[float]
 Population = List[Genome]
-FitnessFunc = Callable[[Genome], float]
+Fitness = Callable[[Genome], float]
 
 
-def foo(x, y, z) -> float:
-    """The function that we want to optimize"""
+def equation(x: float, y: float, z: float) -> float:
+    """The function that we want
+    to optimize a solution for"""
 
-    return 9 * x**3 + 3 * y**2 + 12 * z
+    return 9 * x**3 + 12 * y**2 + z - 25
 
 
 def fitness(genome: Genome) -> float:
-    """Determine the fitness of the genome"""
+    """Determine how much a solution
+    is good for our problem"""
 
-    ans = foo(genome[0], genome[1], genome[2])
+    x, y, z = genome
+    ans = equation(x, y, z)
     if ans == 0:
-        return math.inf
-
+        return inf
     return abs(1/ans)
 
 
 def generate_genome() -> Genome:
-    """Generate genome with random values"""
+    """Generate a genome with random values"""
 
-    return [random.uniform(0, 10000) for _ in range(3)]
+    genome = [random.uniform(0, 10000) for _ in range(3)]
+    return genome
 
 
-def generate_population(length: int) -> Population:
-    """Generate random population with custom length"""
+def generate_population(k: int) -> Population:
+    """Make generation 0 with k genomes"""
 
-    return [generate_genome() for _ in range(length)]
+    generation = [generate_genome() for _ in range(k)]
+    return generation
 
 
 def natural_selection(population: Population, k: int,
-                      fitness_func: FitnessFunc) -> Population:
-    """Select most suitable top k genomes"""
+                      fitness_func: Fitness) -> Population:
+    """Select best k solution from a
+       generation based on genomes' fitness"""
 
     population.sort(key=fitness_func, reverse=True)
+
+    if k > len(population) or k <= 0:
+        return population
+
     return population[:k]
 
 
 def select_parents(population: Population) -> List[Genome]:
-    """Select two parents for the new genomes"""
+    """Select two parents from a population"""
 
-    return random.choices(population=population, k=2)
+    parents = random.choices(population=population, k=2)
+    return parents
 
 
 def crossover(parents: List[Genome], k: int) -> List[Genome]:
-    """Generate new genomes using two parents"""
+    """Generate k genomes from two parents
+    with random portion from each parent's dna"""
 
-    children = []
+    genomes = []
     for _ in range(k):
         genome = [parents[random.choice([0, 1])][i] for i in range(3)]
-        children.append(genome)
+        genomes.append(genome)
 
-    return children
+    return genomes
 
 
 def mutation(genome: Genome) -> Genome:
-    """Mutate a genome to make it unique"""
+    """Change point(s) is genome's dna to achieve variety"""
 
-    for i, _ in enumerate(genome):
-        genome[i] = genome[i] * random.uniform(0.99, 1.01)
-
+    genome = [value * random.uniform(0.99, 1.01) for value in genome]
     return genome
 
 
-def make_generation(population: Population) -> Population:
-    """Make new generation from current generation"""
+def make_generation(population: Population, k: int) -> Population:
+    """Make new generation based on current generation with k genomes"""
 
     generation = []
-    for _ in range(400):
+    genomes_from_parents = 3
+    for _ in range(k // genomes_from_parents):
         parents = select_parents(population=population)
-        generation += crossover(parents=parents, k=3)
+        genomes = crossover(parents=parents, k=genomes_from_parents)
+        generation += genomes
 
-    for i, _ in enumerate(generation):
-        generation[i] = mutation(generation[i])
-
+    generation = [mutation(genome) for genome in generation]
     return generation
 
 
-def is_good_enough(solution: Genome) -> bool:
-    """Determine if a solution is good enough"""
+def is_good_enough(genome: Genome, goal: int,
+                   fitness_func: Fitness) -> bool:
+    """Check if generation's best genome
+    is good enough for our problem"""
 
-    if fitness(solution) > 20000:
+    if fitness_func(genome) >= goal:
         return True
 
     return False
 
 
+def evolution(fitness_func: Fitness, goal: int) -> List[float]:
+    """Evolution function"""
+
+    gen = 0
+    generation = generate_population(k=1200)
+    gen_fitness = []
+    while True:
+        top_genomes = natural_selection(population=generation,
+                                        k=100, fitness_func=fitness)
+        best_genome = top_genomes[0]
+        gen_fitness.append(fitness_func(best_genome))
+
+        print(f"Generation {gen}'s best solution is {
+              fitness_func(best_genome)}, {best_genome}")
+
+        if is_good_enough(genome=best_genome, goal=goal,
+                          fitness_func=fitness):
+            return gen_fitness
+
+        gen += 1
+        generation = make_generation(population=top_genomes, k=1200)
+
+
 def main():
     """Driver function"""
 
-    gen = 0
-    population = generate_population(length=1200)
-    while True:
-        population = natural_selection(
-            population=population, k=100, fitness_func=fitness)
-
-        best_solution = population[0]
-
-        print(f"Generation {gen}'s best solution is {
-              fitness(best_solution)}, {best_solution}")
-
-        if is_good_enough(best_solution):
-            return
-
-        gen += 1
-        population = make_generation(population=population)
+    y = evolution(fitness_func=fitness, goal=200000)
+    x = list(range(len(y)))
+    plt.scatter(x, y)
+    plt.show()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
